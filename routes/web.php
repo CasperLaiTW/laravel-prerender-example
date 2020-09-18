c03c41d3342b9b84f89cbb31192de6cdf877de4f
Casper Lai <casper.lai@sleepingdesign.com>
2020-09-18 09:54:04 +0800

    Cached welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use CodebarAg\LaravelPrerender\PrerenderMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(PrerenderMiddleware::class);
